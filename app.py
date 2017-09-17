79357cd577139b9ed0030c613ed4e37ffc265b24
deljuven <delforzajuven@gmail.com>
2017-09-17 14:06:58 +0800

    first commit for organization

---
+++ app.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from utils import input
+
+if __name__ == '__main__':
+    pass
+    # content = scrapy.retrieve_data(URL)
+    # print scrapy.extract_price(content)
+    input.hi()
